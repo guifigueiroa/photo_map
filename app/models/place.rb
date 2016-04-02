09f3cc707cef06355222f5a63ba7c374c72afc7b
guifigueiroa <guilherme.mfigueiroa@gmail.com>
2016-04-02 10:22:31 -0400

    Create standard queries for places

--- app/models/place.rb
+++ app/models/place.rb
@@ -10,7 +10,7 @@ class Place
     end
     
     @formatted_address = params[:formatted_address]
-    @location = params[:geometry][:geolocation]
+    @location = Point.new(params[:geometry][:geolocation])
   end
   
   def self.mongo_client
@@ -25,5 +25,41 @@ class Place
     hash = JSON.parse(file.read)
     places = Place.collection
     places.insert_many(hash)
+  end
+  
+  def self.find_by_short_name(short_name)
+    collection.find({"address_components.short_name" => short_name})
+  end
+  
+  def self.find(id)
+    id = BSON::ObjectId.from_string(id)
+    place = collection.find(_id: id).first
+    Place.new(place) if !place.nil?
+  end
+  
+  def self.all(offset=0, limit=nil)
+    if limit.nil?
+      places = collection.find.skip(offset)
+    else
+      places = collection.find.skip(offset).limit(limit)  
+    end
+    
+    objs = []
+    places.each do |place| 
+      objs << Place.new(place)
+    end
+    objs
+  end
+  
+  def self.to_places(collection)
+    places = []
+    collection.each do |place| 
+      places << Place.new(place)
+    end
+    places
+  end
+  
+  def destroy
+    self.class.collection.find(_id: BSON::ObjectId(@id)).delete_one
   end
 end
